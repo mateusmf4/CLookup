c88d12be2ec5ec97889bee64afd0bd447767e8fa
Hebert Laurentino dos Santos <hebert.santos343@gmail.com>
2024-08-20 18:01:31 -0300

    Options on MenuPrincipal!

--- CLookuP_Haskell/app/Modules/MenuPrincipal.hs
+++ CLookuP_Haskell/app/Modules/MenuPrincipal.hs
@@ -1,13 +1,26 @@
 module Modules.MenuPrincipal where
+import System.Exit (exitSuccess)
 
 printar :: IO()
 printar = do
-    putStrLn ("--------------------" ++ " CPBooking " ++ "--------------------\n")
+    putStrLn ("\n--------------------" ++ " CPBooking " ++ "--------------------\n")
     putStrLn "Digite a opção desejada:\n"
-    putStrLn "1- Cadastro"
-    putStrLn "2- Visualizar reservas"
-    putStrLn "3- Disponibilidade de reservas" 
-    putStrLn "4- Reservar de sala" 
-    putStrLn "5- Cancelamento de reserva" 
-    putStrLn "6- Sair\n" 
+    putStrLn "1- Login"
+    putStrLn "2- Cadastro"
+    putStrLn "3- Sair\n"
     putStrLn "Digite a opção: "
+    opcao <- getLine
+    opcaoSelecionada opcao
+    printar
+
+opcaoSelecionada :: String -> IO()
+opcaoSelecionada opcao
+--    | opcao == "1" = Modules.Controllers.LoginController.logar
+--    | opcao == "2" = Modules.Controllers.CadastroController.cadastrar
+    | opcao == "3" = sair
+    | otherwise = putStrLn "Error: Opção Inválida!\n"
+
+sair :: IO()
+sair = do
+    putStrLn "Até Mais!"
+    exitSuccess
