6e3a47cd6d4943564225be9f5131d88401a36a20
danieleolivs <danieleoliveira7712@gmail.com>
2024-08-21 20:39:24 -0300

    ajustes: model Estudante

--- haskell/app/Models/Estudante.hs
+++ haskell/app/Models/Estudante.hs
@@ -1,13 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Models.Estudante where
 
 import GHC.Generics
+import Data.Version
 import Data.Aeson
 
 data Estudante = Estudante {
     nome :: String,
     matricula :: Int,
-    prioridade :: Int
+    monitor :: Bool
 } deriving (Generic, Show)
 
 instance ToJSON Estudante
-instance FromJSON Estudante
+instance FromJSON Estudante where
+    parseJSON = withObject "Estudante" $ \v -> Estudante 
+        <$> v .: "nome"
+        <*> v .: "matricula"
+        <*> v .: "monitor"
