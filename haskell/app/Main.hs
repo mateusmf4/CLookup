273a73236388f9a12dd4d1c150e13044644118b9
Mateus <130506942+mateusmf4@users.noreply.github.com>
2024-08-21 14:18:54 -0300

    restruturar codigo pra pasta Models

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,3 +1,30 @@
-import Modules.MenuPrincipal
+module Main where
 
-main = Modules.MenuPrincipal.printar
+import System.Exit (exitSuccess)
+
+printar :: IO()
+printar = do
+    putStrLn ("\n--------------------" ++ " CPBooking " ++ "--------------------\n")
+    putStrLn "Digite a opcao desejada:\n"
+    putStrLn "1- Login"
+    putStrLn "2- Cadastro"
+    putStrLn "3- Sair\n"
+    putStrLn "Digite a opcao: "
+    opcao <- getLine
+    opcaoSelecionada opcao
+    printar
+
+opcaoSelecionada :: String -> IO()
+opcaoSelecionada opcao
+    -- | opcao == "1" = Modules.Controllers.LoginController.logar
+    -- | opcao == "2" = Modules.Controllers.CadastroController.cadastrar
+    | opcao == "3" = sair
+    | otherwise = putStrLn "Error: Opcao Invalida!\n"
+
+sair :: IO()
+sair = do
+    putStrLn "Até Mais!"
+    exitSuccess
+
+main :: IO()
+main = printar
