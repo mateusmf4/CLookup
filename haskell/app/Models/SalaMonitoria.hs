b9266a3c56806826b18e3287cb0c4e60c945b45c
Maria Clara <guedesoliveira004@gmail.com>
2024-08-24 21:47:33 -0300

    campo de horario da reserva da sala de monitoria

--- haskell/app/Models/SalaMonitoria.hs
+++ haskell/app/Models/SalaMonitoria.hs
@@ -2,11 +2,13 @@ module Models.SalaMonitoria where
 
 import GHC.Generics
 import Data.Aeson
+import Data.Time (UTCTime)
 
 data SalaMonitoria = SalaMonitoria {
     nome :: String,
     numeroSala :: Int,
-    ocupada :: Bool -- adicionei esse campo para verificar se uma sala está ocupada ou não p poder trabalhar com o resultado no controller
+    ocupada :: Bool, -- adicionei esse campo para verificar se uma sala está ocupada ou não p poder trabalhar com o resultado no controller
+    reserva :: Maybe UTCTime -- campo para o horário da reserva ou nada se não tiver reservada
 } deriving (Generic, Show)
 
 instance ToJSON SalaMonitoria
