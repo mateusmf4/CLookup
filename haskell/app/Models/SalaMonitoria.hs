609d9e05a320fdcd7086dd2a13371ce098ed40e6
Maria Clara <guedesoliveira004@gmail.com>
2024-08-25 14:08:25 -0300

    mudanca no model de sala de monitoria

--- haskell/app/Models/SalaMonitoria.hs
+++ haskell/app/Models/SalaMonitoria.hs
@@ -7,9 +7,17 @@ import Data.Time (UTCTime)
 data SalaMonitoria = SalaMonitoria {
     nome :: String,
     numeroSala :: Int,
-    ocupada :: Bool, -- adicionei esse campo para verificar se uma sala está ocupada ou não p poder trabalhar com o resultado no controller
-    reserva :: Maybe UTCTime -- campo para o horário da reserva ou nada se não tiver reservada
+    reservas :: [Reserva]   -- Lista de reservas para a sala. Por meio de cada horario e data agendada, eu poderia verificar se já 
+                            -- tem reserva para esse dia e horário ou se ta livre
+} deriving (Generic, Show)
+
+data Reserva = Reserva {
+    inicio :: UTCTime,  -- data e horário do início de uma reserva
+    termino :: UTCTime  -- data e horário do término de uma reserva
 } deriving (Generic, Show)
 
 instance ToJSON SalaMonitoria
 instance FromJSON SalaMonitoria
+
+instance ToJSON Reserva
+instance FromJSON Reserva
