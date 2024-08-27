ab9e99ada339ce5b70c4bbb4ea4509c07dfb6b16
Mateus <130506942+mateusmf4@users.noreply.github.com>
2024-08-27 19:33:12 -0300

    ajeitar unicode no windows
    permite imprimir caracteres unicode sem lançar exceção

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,10 +1,20 @@
 module Main where
 
 import Menus.Inicio (menuInicio)
-import System.IO (hSetBuffering, BufferMode (NoBuffering), stdout)
+import System.IO (hSetBuffering, BufferMode (NoBuffering), stdout, hGetEncoding, mkTextEncoding, hSetEncoding, stderr, stdin, Handle)
+
+makeSafe :: Handle -> IO ()
+makeSafe h = do
+  ce' <- hGetEncoding h
+  case ce' of
+    Nothing -> return ()
+    Just ce -> mkTextEncoding (takeWhile (/= '/') (show ce) ++ "//TRANSLIT") >>=
+      hSetEncoding h
 
 main :: IO()
 main = do
     -- permite imprimir e pegar o input na mesma linha
     hSetBuffering stdout NoBuffering
+    -- permite imprimir caracteres unicode no windows
+    mapM_ makeSafe [stdout, stdin, stderr]
     menuInicio
