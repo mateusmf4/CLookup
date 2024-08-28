5e245a2fdd7b129d20cc684da50793a111c2d1ae
gabysouzz <gabrielly.souza.lima@ccc.ufcg.edu.br>
2024-08-28 00:12:54 -0300

    adicionando comentários

--- haskell/app/Controllers/SalaController.hs
+++ haskell/app/Controllers/SalaController.hs
@@ -3,22 +3,27 @@ import qualified Repository
 import Data.Maybe (isJust, isNothing)
 import Models.Sala (Sala)
 
+-- Compara duas reservas e verifica se há conflito de horários.
 verificaConflito :: Reserva -> Reserva -> Bool
 verificaConflito r1 r2 = not (termino r1 <= inicio r2 || termino r2 <= inicio r1)
 
+-- Compara duas reservas e verifica se são iguais.
 reservasIguais :: Reserva -> Reserva -> Bool
 reservasIguais r1 r2 = inicio r1 == inicio r2 && termino r1 == termino r2
 
+-- Verificando se uma determinada sala esta disponível.
 disponibilidadeSala :: Reserva -> Sala -> Bool
 disponibilidadeSala novaReserva sala = 
     not $ any (verificaConflito novaReserva) 
     (reservas sala)
 
+-- Cancelando uma reserva de Sala.
 cancelarReserva :: Reserva -> Sala -> Sala
 cancelarReserva reserva sala = sala { reservas = novasReservas }
   where
     novasReservas = deleteBy reservasIguais reserva (reservas sala)
 
+-- Criando salas fixas do Bloco 
 salasMonitoria :: [Models.Sala]
 salasMonitoria =
     [
