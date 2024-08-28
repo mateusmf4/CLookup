fb6bab8a5bef7718537f8725ff3c53449ac725ce
Maria Clara <guedesoliveira004@gmail.com>
2024-08-27 23:21:41 -0300

    criacao de sala monitoria

--- haskell/app/Controllers/SalaController.hs
+++ haskell/app/Controllers/SalaController.hs
@@ -8,18 +8,25 @@ selecionaSala :: String -> Sala -> Bool
 selecionaSala nomeDesejado sala
 nome sala = nomeDesejado
 
+salasMonitoria :: [Models.Sala]
+salasMonitoria =
+    [
+        Sala {nome = "Patos", qtdeComputador = 0, qtdeCadeiras = 10, tipoSala=0, reservas=[]},
+        Sala {nome = "Cuités", qtdeComputador = 0, qtdeCadeiras = 10, tipoSala=0, reservas=[]},
+        Sala {nome = "Sousa", qtdeComputador = 0, qtdeCadeiras = 10, tipoSala=0, reservas=[]}
+    ]
 salasLCC :: [Models.Sala]
 salasLCC = 
     [
-        Sala {nome = "LCC 1", numeroSala = 4, qtdeComputador = 40, qtdeCadeiras = 50, tipoSala=1, reservas=[]},
-        Sala {nome = "LCC 2", numeroSala = 5, qtdeComputador = 40, qtdeCadeiras = 50, tipoSala=1, reservas=[]},
-        Sala {nome = "LCC 3", numeroSala = 6, qtdeComputador = 130, qtdeCadeiras = 145, tipoSala=1, reservas=[]}
+        Sala {nome = "LCC 1", qtdeComputador = 40, qtdeCadeiras = 50, tipoSala=1, reservas=[]},
+        Sala {nome = "LCC 2", qtdeComputador = 40, qtdeCadeiras = 50, tipoSala=1, reservas=[]},
+        Sala {nome = "LCC 3", qtdeComputador = 130, qtdeCadeiras = 145, tipoSala=1, reservas=[]}
 
     ]
 salaAula :: [Models.Sala]
 salaAula = 
     [
-        Sala {nome = "CP-01", numeroSala = 7, qtdeComputador = 0, qtdeCadeiras = 45, tipoSala = 2, reservas = []}
+        Sala {nome = "CP-01", qtdeComputador = 0, qtdeCadeiras = 45, tipoSala = 2, reservas = []}
     ]
 -- método: reservar uma sala -- maria
 -- método: verificar as salas disponíveis e indisponíveis -- gaby
