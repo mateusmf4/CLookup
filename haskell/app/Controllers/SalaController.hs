85c5794b5ad681b93c7f810bd64502e6e29973e2
Maria Clara <guedesoliveira004@gmail.com>
2024-08-28 00:30:43 -0300

    sincronizando

--- haskell/app/Controllers/SalaController.hs
+++ haskell/app/Controllers/SalaController.hs
@@ -23,14 +23,14 @@ cancelarReserva reserva sala = sala { reservas = novasReservas }
   where
     novasReservas = deleteBy reservasIguais reserva (reservas sala)
 
-salasMonitoria :: [Models.Sala]
+salasMonitoria :: [Sala]
 salasMonitoria =
     [
         Sala {nome = "Patos", qtdeComputador = 0, qtdeCadeiras = 10, tipoSala=0, reservas=[]},
         Sala {nome = "Cuités", qtdeComputador = 0, qtdeCadeiras = 10, tipoSala=0, reservas=[]},
         Sala {nome = "Sousa", qtdeComputador = 0, qtdeCadeiras = 10, tipoSala=0, reservas=[]}
     ]
-salasLCC :: [Models.Sala]
+salasLCC :: [Sala]
 salasLCC = 
     [
         Sala {nome = "LCC 1", qtdeComputador = 40, qtdeCadeiras = 50, tipoSala=1, reservas=[]},
@@ -38,7 +38,7 @@ salasLCC =
         Sala {nome = "LCC 3", qtdeComputador = 130, qtdeCadeiras = 145, tipoSala=1, reservas=[]}
 
     ]
-salaAula :: [Models.Sala]
+salaAula :: [Sala]
 salaAula = 
     [
         Sala {nome = "CP-01", qtdeComputador = 0, qtdeCadeiras = 45, tipoSala = 2, reservas = []}
