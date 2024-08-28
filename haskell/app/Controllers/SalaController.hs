71b9dea7966ff736ab26951f6ca5e0d95fa0ea2d
Maria Clara <guedesoliveira004@gmail.com>
2024-08-27 21:54:09 -0300

    controller teste

--- haskell/app/Controllers/SalaController.hs
+++ haskell/app/Controllers/SalaController.hs
@@ -1,1 +1,16 @@
 module Controllers.SalaController where
+import qualified Repository
+import Data.Maybe (isJust, isNothing)
+import Models.Sala (Sala)
+
+
+
+selecionaSala :: String -> Sala -> Bool
+selecionaSala nomeDesejado sala
+nome sala = nomeDesejado
+
+
+-- método: reservar uma sala -- maria
+-- método: verificar as salas disponíveis e indisponíveis -- gaby
+-- método: cancelar uma reserva -- gaby
+-- método: atualizar uma reserva -- maria
