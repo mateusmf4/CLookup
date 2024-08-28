4c1c476637e1879770f77256cbca2853755e0a21
Hebert Laurentino dos Santos <hebert.santos343@gmail.com>
2024-08-27 22:58:05 -0300

    Cadastro do Professor!

--- haskell/app/Controllers/ProfessorController.hs
+++ haskell/app/Controllers/ProfessorController.hs
@@ -1,1 +1,14 @@
 module Controllers.ProfessorController where
+import Models.Professor
+import Data.Maybe (isJust)
+import Repository
+
+cadastro :: String -> Int -> IO (Either String Professor)
+cadastro nome matricula = do
+    existe <- isJust <$> Repository.fetchProfessor matricula
+    if existe then do
+        return $ Left "Professor com mesma matricula ja existe!"
+    else do
+        let professor = Professor { nomeProfessor = nome, matriculaProfessor = matricula }
+        Repository.saveProfessor professor
+        return $ Right professor
