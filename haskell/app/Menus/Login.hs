1bd82f43757e0968d7db2af621500da26eaeae05
Hebert Laurentino dos Santos <hebert.santos343@gmail.com>
2024-08-28 01:19:54 -0300

    Login atualizado!

--- haskell/app/Menus/Login.hs
+++ haskell/app/Menus/Login.hs
@@ -4,23 +4,25 @@ import Menus.Util (printBanner, readLnPrompt)
 import Menus.Logado (menuLogado)
 import Repository (fetchEstudante, fetchProfessor)
 import Data.Maybe (isJust)
+import Models.Estudante (Estudante)
+import Models.Professor (Professor)
 
 menuLogin :: IO()
 menuLogin = do
     printBanner
     matricula <- readLnPrompt "Digite sua matrícula: "
-    search <- searchUser matricula
-    if search then do
-        menuLogado True
+    estudante <- studentUser matricula
+    professor <- professorUser matricula
+    if isJust estudante then do
+        menuLogado estudante
+    else if isJust professor then do
+        menuLogado professor
     else do
         putStrLn "Não existe usuário com essa matrícula!"
         return()
 
-searchUser :: Int -> IO Bool
-searchUser matricula = do
-    existeEstudante <- isJust <$> Repository.fetchEstudante matricula
-    existeProfessor <- isJust <$> Repository.fetchProfessor matricula
-    if existeEstudante || existeProfessor then do
-        return True
-    else do
-        return False
+studentUser :: Int -> IO (Maybe Estudante)
+studentUser matricula = Repository.fetchEstudante matricula
+
+professorUser :: Int -> IO (Maybe Professor)
+professorUser matricula = Repository.fetchProfessor matricula
