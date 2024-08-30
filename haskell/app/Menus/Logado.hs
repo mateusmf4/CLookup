6889efc17c63d8dd2f1fee231807b0d902b027aa
gabysouzz <gabrielly.souza.lima@ccc.ufcg.edu.br>
2024-08-30 18:10:10 -0300

    adicionando menu de tornar monitor

--- haskell/app/Menus/Logado.hs
+++ haskell/app/Menus/Logado.hs
@@ -1,5 +1,6 @@
 module Menus.Logado where
-import Menus.Util (printMenuEscolhas)
+
+import Menus.Util (printMenuEscolhas, getLinePrompt)
 import System.Exit (exitSuccess)
 
 import qualified Controllers.SalaController as SalaController
@@ -7,15 +8,23 @@ import Control.Monad (forM_)
 import Utils (enumerate)
 import Models.Sala
 import Models.Usuario
+import Controllers.EstudanteController (atualizaMonitor)
 
 menuLogado :: Usuario -> IO ()
 menuLogado user = do
     putStrLn "Bem vindo ao menu logado\n"
-    printMenuEscolhas [
-        ("Ver Sala", menuVerSala),
-        ("Reservar Sala", return ()),
-        ("Sair", exitSuccess)
-        ]
+    case user of
+        Est _ -> printMenuEscolhas [
+            ("Ver Sala", menuVerSala),
+            ("Reservar Sala", return()),
+            ("Sair", exitSuccess)
+            ]
+        Prof _ ->  printMenuEscolhas [
+            ("Ver Sala", menuVerSala),
+            ("Reservar Sala", return()),
+            ("Tornar usuário monitor", menuMonitor),
+            ("Sair", exitSuccess)
+            ]
     menuLogado user
 
 menuVerSala :: IO ()
@@ -23,3 +32,12 @@ menuVerSala = do
     salas <- SalaController.listarSalas
     forM_ (enumerate salas) $ \(i, sala) -> do
         putStrLn $ (show (i + 1)) ++ ". " ++ (nomeSala sala)
+
+menuMonitor :: IO()
+menuMonitor = do
+    matricula <- getLinePrompt "Informe a matricula do aluno: "
+    valor <- atualizaMonitor (read matricula) True
+    case valor of
+        Left erro -> putStrLn erro
+        Right _ -> do
+            putStrLn "Monitor adicionado!\n"
