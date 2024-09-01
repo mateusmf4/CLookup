748768e3325a160bb8794cbb52a3c039116fd58f
Mateus <130506942+mateusmf4@users.noreply.github.com>
2024-09-01 19:00:29 -0300

    documentação restante

--- haskell/app/Controllers/ProfessorController.hs
+++ haskell/app/Controllers/ProfessorController.hs
@@ -1,8 +1,11 @@
+-- ProfessorController contem as funções para um professor no sistema.
 module Controllers.ProfessorController where
+
 import Models.Professor
 import Data.Maybe (isJust)
 import qualified Repository
 
+-- Realiza o cadastro de um professor no sistema, atravez de uma matricula e um nome.
 cadastro :: String -> Int -> IO (Either String Professor)
 cadastro nome matricula = do
     existe <- isJust <$> Repository.fetchUsuario matricula
--- haskell/app/Models/Professor.hs
+++ haskell/app/Models/Professor.hs
@@ -1,12 +1,15 @@
+-- modulo que inclui o model do professor no sistema
 module Models.Professor where
 
 import GHC.Generics
 import Data.Aeson
 
+-- Tipo que representa um professor, com nome e matricula.
 data Professor = Professor {
     nomeProfessor :: String,
     matriculaProfessor :: Int
 } deriving (Generic, Show)
 
+-- Permite transformar o professor em um objeto JSON.
 instance ToJSON Professor
 instance FromJSON Professor
