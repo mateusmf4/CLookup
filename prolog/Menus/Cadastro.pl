28268cab82d30e397343abd4e20f750a2b7c64d0
Hebert Laurentino <hebert.laurentino.santos@ccc.ufcg.edu.br>
2024-09-23 16:05:24 -0300

    Cadastro funcionando.

--- prolog/Menus/Cadastro.pl
+++ prolog/Menus/Cadastro.pl
@@ -1,6 +1,9 @@
 :- module(cadastro, [menuCadastro/0]).
+:- use_module('Utils.pl').
+:- use_module('./Repository.pl').
+:- use_module('./Models/Usuario.pl').
 
-cadastro :-
+cadastro:-
     writeln("╔══════════════════════════════════════════════════════════╗"),
     writeln("║         ____          _           _                      ║"),
     writeln("║        / ___|__ _  __| | __ _ ___| |_ _ __ ___           ║"),
@@ -9,6 +12,38 @@ cadastro :-
     writeln("║        |____|__,_||__,_||__,_|___/|__|_|  |___/          ║"),
     writeln("╚══════════════════════════════════════════════════════════╝").
 
-menuCadastro :- 
+menuCadastro:- 
     cadastro,
-    writeln("Cadastrar como:\n").
+    writeln("Cadastrar como:\n"),
+    writeln("1. Estudante"), writeln("2. Professor"), writeln("3. Voltar"),
+    readNumber(I),
+    (I == 1 -> cadastroEstudante; I == 2 -> cadastroProfessor; I == 3 -> menuInicio; writeln("Erro: Opção inválida!")),
+    menuCadastro.
+
+cadastroEstudante:-
+    writeln("Nome: "), readStr(N),
+    writeln("Matrícula: "), readNumber(M),
+    atom_length(M, Length),
+    (Length =:= 9 ->
+        (verifyUserExists(M) == 0 -> 
+            newUsuario(M, N, estudante, User),
+            saveUsuario(User),
+            writeln("Cadastro realizado com sucesso!");
+            writeln("Usuário com mesma matricula já existe!")
+        );
+    writeln("Matrícula inválida!")),
+    menuInicio.
+
+cadastroProfessor:-
+    writeln("Nome: "), readStr(N),
+    writeln("Matrícula: "), readNumber(M),
+    atom_length(M, Length),
+    (Length =:= 9 ->
+        (verifyUserExists(M) == 0 -> 
+            newUsuario(M, N, professor, User),
+            saveUsuario(User),
+            writeln("Cadastro realizado com sucesso!");
+            writeln("Usuário com mesma matricula já existe!")
+        );
+    writeln("Matrícula inválida!")),
+    menuInicio.
