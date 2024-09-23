6966c9b2554a2eb5a1a4573fb518e7b20c6eb308
Hebert Laurentino <hebert.laurentino.santos@ccc.ufcg.edu.br>
2024-09-23 13:03:02 -0300

    Redirecionamento do início para Login, Cadastro e sair

--- prolog/Menus/Inicio.pl
+++ prolog/Menus/Inicio.pl
@@ -1,8 +1,32 @@
 :- module(inicio, [menuInicio/0]).
 
-:- use_module('Menus/Utils.pl').
+:- use_module('Utils.pl').
+:- use_module('Login.pl').
+:- use_module('Cadastro.pl').
 
-menuInicio :-
-    write("Digite algo: "),
-    readStr(N),
-    format("Digitou ~w\n", [N]).
+menu:- 
+    writeln("╔═══════════════════════════════════════════════════════════════════════════════╗"),
+    writeln("║                                                                               ║"),
+    writeln("║                 ______  __                 __               _______           ║"),
+    writeln("║               .' ___  |[  |               [  |  _          |_   __ |          ║"),
+    writeln("║             / .'   |_| | |  .--.    .--.  | | / ] __   _    | |__) |          ║"),
+    writeln("║             | |        | |/ .'`| |/ .'`| || '' < [  | | |   |  ___/           ║"),
+    writeln("║             | `.___.'| | || |__. || |__. || |`| | | |_/ |, _| |_              ║"),
+    writeln("║              `.____ .'[___]'.__.'  '.__.'[__|  |_]'.__.'_/|_____|             ║"),
+    writeln("║                                                                               ║"),
+    writeln("║       Menu:                                                                   ║"),
+    writeln("║          1. Login                                                             ║"),
+    writeln("║          2. Cadastro                                                          ║"),
+    writeln("║          3. Sair                                                              ║"),
+    writeln("║                                                                               ║"),
+    writeln("║                                                                               ║"),
+    writeln("║       Digite a opção:                                                         ║"),
+    writeln("╚═══════════════════════════════════════════════════════════════════════════════╝").
+
+menuInicio :- 
+    menu,
+    readNumber(I), 
+    (I == 1 -> menuLogin; I == 2 -> cadastro; I == 3 -> sair; writeln("Erro: Opção invalida!")),
+    menuInicio.
+
+sair:- writeln("Até mais!"), halt(0).
