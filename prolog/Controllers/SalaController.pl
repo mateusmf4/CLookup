d402e41a76a425a8b628fa2e621de07fa2618385
Maria Clara <guedesoliveira004@gmail.com>
2024-09-30 13:32:25 -0300

    reserva_conflitante e se_reserva_conflita

--- prolog/Controllers/SalaController.pl
+++ prolog/Controllers/SalaController.pl
@@ -1,1 +1,7 @@
-% apenas p criar o diretorio de controllers
+:- use_module(library(date)), use_module(models([usuario, sala, reserva])).
+
+reserva_conflitantes(Reserva, Reservas, Conflitantes) :-
+    findall(Reserva, (member(Reserva, Reservas), se_reserva_conflita(Reserva, Reserva)), Conflitantes).
+
+se_reserva_conflita(Reserva1(_, _, Inicio1, Termino1), Reserva2(_, _, Inicio2, Termino2), Conflito) :-
+    Conflito is (not (Termino1 =< Inicio2 or Inicio1 >= Termino2)).
