147472668dbb11eade36e7f2d39868372dfd84be
Maria Clara <guedesoliveira004@gmail.com>
2024-10-04 23:02:39 -0300

    correção na verificacao de reservas conflitantes

--- prolog/Controllers/SalaController.pl
+++ prolog/Controllers/SalaController.pl
@@ -1,13 +1,15 @@
-:- module(sala_controller, [listar_salas/1, get_sala/2]).
+:- module(sala_controller, [listar_salas/1, get_sala/2, findall/3, reserva_conflitantes/3, se_reserva_conflita/2]).
 
 :- use_module(library(date)).
 :- use_module('Repository.pl').
 
-% reserva_conflitantes(Reserva, Reservas, Conflitantes) :-
-%     findall(Reserva, (member(Reserva, Reservas), se_reserva_conflita(Reserva, Reserva)), Conflitantes).
+reserva_conflitantes(NovaReserva, Reservas, Conflitantes) :-
+    findall(R, (member(R, Reservas), se_reserva_conflita(NovaReserva, R)), Conflitantes).
 
-% se_reserva_conflita(Reserva1(_, _, Inicio1, Termino1), Reserva2(_, _, Inicio2, Termino2), Conflito) :-
-%     Conflito is (not (Termino1 =< Inicio2 or Inicio1 >= Termino2)).
+se_reserva_conflita(Reserva1, Reserva2) :-
+    Reserva1 = reserva(_, Inicio1, Termino1),
+    Reserva2 = reserva(_, Inicio2, Termino2),
+    not((Termino1 =< Inicio2; Inicio1 >= Termino2)).
 
 % Lista todas as salas cadastradas.
 listar_salas(Salas) :-
